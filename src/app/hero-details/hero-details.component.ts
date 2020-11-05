608e4ed89a73f93a283eb2182cce80729ed59547
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-11-05 07:47:56 +0600

    App Updated

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.scss']
+})
+export class HeroDetailsComponent implements OnInit {
+
+  constructor() { }
+  @Input() hero:Hero;
+
+  ngOnInit(): void {
+  }
+
+}
